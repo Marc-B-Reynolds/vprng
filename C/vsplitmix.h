03a6798664ac23ef7e1a60149aa378da669855d2
Marc B. Reynolds <Marc-B-Reynolds@users.noreply.github.com>
2025-02-25 20:07:36 +0100

    Create vsplitmix.h

---
+++ C/vsplitmix.h
@@ -0,0 +1,36 @@
+// Marc B. Reynolds, 2025
+// Public Domain under http://unlicense.org, see link for details.
+
+// example customization header for minimum targets w/supported
+// SIMD 64-bit products. This make an off-the-shelf SplitMix64
+// that's four wide. Difference vs. the implementation in the
+// standard Java library (java.util.SplittableRandom) are:
+// * since scalar Java discards bits for 32-bit results. It'd be
+//   a loss to 'cache' and here it's for free. It also uses
+//   a hybrid method for finalizing. Since it's dropping 32-bit
+//   it uses a cheaper finalizer (MIX13)
+// * different method of selecting the additive constant to
+//   parameterize generators.
+//
+// Ideally this should be "more" different. I'd be much happier
+// with four unique finalizers BUT the trick is finding a good
+// set which all have the same shift amounts.
+
+#pragma once
+
+#define VPRNG_NAME "vsplitmix"
+#define VPRNG_MIX_EXTERNAL
+
+#include "vprng.h"
+
+// MIX14: http://zimbry.blogspot.com/2011/09/better-bit-mixing-improving-on.html
+static inline u32x8_t vprng_mix(u64x4_t x)
+{
+  x ^= x >> 30; x *= 0x4be98134a5976fd3;
+  x ^= x >> 29; x *= 0x3bc0993a5ad19a13;
+  x ^= x >> 31;
+
+  return vprng_cast_u32(x);
+}
+
+
